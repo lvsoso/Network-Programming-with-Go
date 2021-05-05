fff8d7cd64f6489fcff9e3f016e410725fb1b8fa
lvsoso <wuqize5109@163.com>
2021-05-06 00:14:51 +0800

    add: gob encode

---
+++ chapter4/save_gob.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+)
+
+type Person struct {
+	Name  Name
+	Email []Email
+}
+
+type Name struct {
+	Family   string
+	Personal string
+}
+
+type Email struct {
+	Kind    string
+	Address string
+}
+
+func main() {
+	person := Person{
+		Name: Name{Family: "nm", Personal: "j"},
+		Email: []Email{Email{Kind: "home", Address: "j@nm.name"},
+			Email{Kind: "work", Address: "j.nm@163.com"}}}
+	saveGob("person.gob", person)
+}
+
+func saveGob(fileName string, key interface{}) {
+	outFile, err := os.Create(fileName)
+	checkError(err)
+	encoder := gob.NewEncoder(outFile)
+	err = encoder.Encode(key)
+	checkError(err)
+	outFile.Close()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
